4caee5bffb26ada701d1bea90f35342fb96e4234
branberry <brandon.ly@mongodb.com>
2024-05-28 13:22:07 -0500

    add clone

--- build.sh
+++ build.sh
@@ -5,4 +5,12 @@ echo RUNNING PARSER!
 
 ls -l ./snooty-parser
 chmod +x ./snooty-parser/snooty
-./snooty-parser/snooty/snooty build .
+./snooty-parser/snooty/snooty build . --output=./bundle.zip
+git clone -b v0.16.14 --depth 1 https://github.com/mongodb/snooty.git 
+echo GATSBY_MANIFEST_PATH=$(pwd)/bundle.zip >> ./snooty/.env.production
+cd snooty                
+npm ci --legacy-peer-deps
+git clone --depth 1 https://github.com/mongodb/docs-tools.git
+mkdir -p ./static/images
+mv ./docs-tools/themes/mongodb/static ./static/docs-tools
+mv ./docs-tools/themes/guides/static/images/bg-accent.svg ./static/docs-tools/images/bg-accent.svg

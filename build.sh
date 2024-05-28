350541fa3c5bcbd3ee60e2cdf3bb42d48e2040e0
branberry <brandon.ly@mongodb.com>
2024-05-28 12:16:54 -0500

    Add echo for debugging

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 curl -L -o snooty-parser.zip https://github.com/mongodb/snooty-parser/releases/download/v0.16.6/snooty-v0.16.6-linux_x86_64.zip
 unzip -d ./snooty-parser snooty-parser.zip
+
+echo RUNNING PARSER!
+
 sh ./snooty-parser/snooty build .

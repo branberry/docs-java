7ba50264853ab793da6be994027e77883dbe13b9
branberry <brandon.ly@mongodb.com>
2024-05-28 13:56:54 -0500

    Refactor paths

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ git clone -b v0.16.14 --depth 1 https://github.com/mongodb/snooty.git
 echo GATSBY_MANIFEST_PATH=$(pwd)/bundle.zip >> ./snooty/.env.production
 cd snooty                
 npm ci --legacy-peer-deps
-git clone --depth 1 https://github.com/mongodb/docs-tools.git
-mkdir -p ./static/images
-mv ./docs-tools/themes/mongodb/static ./static/docs-tools
-mv ./docs-tools/themes/guides/static/images/bg-accent.svg ./static/docs-tools/images/bg-accent.svg
+git clone --depth 1 https://github.com/mongodb/docs-tools.git ./snooty/docs-tools
+mkdir -p ./snooty/static/images
+mv ./snooty/docs-tools/themes/mongodb/static ./static/docs-tools
+mv ./snooty/docs-tools/themes/guides/static/images/bg-accent.svg ./static/docs-tools/images/bg-accent.svg

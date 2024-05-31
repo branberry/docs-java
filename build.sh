75611dadb9197febcab7ab118325b0a40ce794aa
branberry <brandon.ly@mongodb.com>
2024-05-31 09:00:09 -0500

    update index

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
 curl -L -o snooty-parser.zip https://github.com/mongodb/snooty-parser/releases/download/v0.16.6/snooty-v0.16.6-linux_x86_64.zip
 unzip -d ./snooty-parser snooty-parser.zip
 
-echo RUNNING PARSER!
-
 ls -l ./snooty-parser
 chmod +x ./snooty-parser/snooty
 ./snooty-parser/snooty/snooty build . --output=./bundle.zip
